d646fcf110c41bfd958efe508f5d7f9e81778404
Colotl-Garista <ocolotl-gar@cps.k12.il.us>
2024-12-16 11:35:50 -0600

    made some cahnges

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-import streamlit as st
-st.write("Hello, World!")
+# import streamlit as st
+# st.write("Hello, World!")
 
-st.title("Title")
-st.header("Header")
-st.subheader("Sub-header")
+# st.title("Title")
+# st.header("Header")
+# st.subheader("Sub-header")
+
+import streamlit as st
+
+# Set the title of the web page
+st.title('Simple Web Form with Streamlit')
+
+# Set a header
+
+# Text input for name
+
+# Dropdown menu for selecting an option
+
+# Slider for selecting a value
+
+# Submit button
+
+# Additional information
+
+# Footer
+
